c9d9bc31ea73fc9e2eb5c62af3f8967183ac02ce
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-02-25 21:49:06 +0100

    Create Firmware.ino

---
+++ Firmware.ino
@@ -0,0 +1,121 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+
+#define SS_PIN 53
+#define RST_PIN 3
+#define LOCK_PIN 2 // Lock-style Solenoid 12VDC
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+SoftwareSerial BTSerial(18, 19); // RX | TX
+
+// Table of accepted NFC tag UIDs
+String UIDs[] = {"B4 B2 70 1C", "2F 6F 36 7A","80 63 2B 32"};
+int unlockAttempts = 0;
+
+void setup() {
+  Serial.begin(9600); // Initialize serial communication
+  BTSerial.begin(9600); // Initialize bluetooth communication
+  SPI.begin(); // Initialize SPI bus
+  mfrc522.PCD_Init(); // Initialize MFRC522
+  pinMode(LOCK_PIN, OUTPUT); // Initialize lock pin
+}
+
+void loop() {
+  // Look for new cards
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    // Check if the card's UID is in the table of accepted UIDs
+    String uid = getUIDAsString(mfrc522.uid.uidByte, mfrc522.uid.size);
+    bool accepted = false;
+    for (int i = 0; i < sizeof(UIDs) / sizeof(UIDs[0]); i++) {
+      if (UIDs[i] == uid) {
+        accepted = true;
+        break;
+      }
+    }
+    if (accepted) {
+
+      unlockAttempts = 0;
+
+      if (digitalRead(LOCK_PIN) == LOW)
+      {
+      Serial.print(F("UID tag : "));
+      Serial.println(uid);
+      Serial.println(F("Access granted"));
+      digitalWrite(LOCK_PIN, HIGH); // Open the lock
+      Serial.print(F("Lock: "));
+      Serial.print(LOCK_PIN);
+      Serial.println(F(" has been opened"));
+      String message = "Access granted, lock "+ LOCK_PIN;
+      message += "opened , UID tag :" + uid;
+      BTSerial.println(message); // Send message via bluetooth
+      delay(1000);
+      }   
+      else {
+      Serial.print(F("UID tag : "));
+      Serial.println(uid);
+      Serial.println(F("Access granted")); 
+      digitalWrite(LOCK_PIN, LOW); // Close the lock     
+      Serial.print(F("Lock: "));
+      Serial.print(LOCK_PIN);  
+      Serial.println(F(" has been closed"));    
+      String message = "Access granted, lock "+ LOCK_PIN;
+      message +=  " closed, UID tag: " + uid;
+      BTSerial.println(message); // Send message via bluetooth
+      delay(1000);
+      }
+    }
+     else {
+      unlockAttempts++;
+      Serial.print(F("UID tag : "));
+      Serial.println(uid);
+      Serial.println(F("Access denied"));
+      String message = "Access denied, UID tag: " + uid;
+      BTSerial.println(message); // Send message via bluetooth
+      if (unlockAttempts == 4) {
+      BTSerial.println("Warning 4 attempts to unlock lock");
+      delay(1000);
+      BTSerial.println("Locking down for 30 seconds");
+      delay(30000);
+      }
+      delay(1000);
+        
+    }
+    }
+
+    String record = sendStatusAndRecords();
+    BTSerial.println(record);
+}
+
+// Convert the UID byte array to a string
+String getUIDAsString(byte *uidBytes, byte uidSize) {
+  String uidString = "";
+  for (byte i = 0; i < uidSize; i++) {
+    if (uidBytes[i] < 0x10) {
+      uidString += "0";
+    }
+    uidString += String(uidBytes[i], HEX);
+    uidString.toUpperCase();
+    uidString += " ";
+  }
+  uidString.trim();
+  return uidString;
+}
+
+
+// Function to send status and records via bluetooth
+String sendStatusAndRecords() {
+  
+  String dateString = "01-01-2000";
+  String timeString = "22:22:22";
+  String dateTimeString = dateString + " " + timeString;
+  // Check if the lock is currently open or closed
+  String lockStatus = digitalRead(LOCK_PIN) == HIGH ? "Open" : "Closed";
+  // Construct a message with the lock status and some example records
+  String record = "Records: \n Lock is " + lockStatus + "\n- " + dateTimeString +"\n";
+  return record;
+  delay(1000);
+  }
+
